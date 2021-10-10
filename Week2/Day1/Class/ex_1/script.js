7485f81c64beada43686fe3b36cc429a60aeeb2c
lior shaked <Liorshak1986@gmail.com>
2021-10-10 12:02:44 +0300

    ex finished

---
+++ Week2/Day1/Class/ex_1/script.js
@@ -0,0 +1,8 @@
+ let addressNumber = 85;
+ let addressStreet= "Manara";
+ let country= "Israel";
+
+
+let global_address=   `i live in ${addressStreet} ${addressNumber}, in ${country}`;
+console.log(global_address);
+
