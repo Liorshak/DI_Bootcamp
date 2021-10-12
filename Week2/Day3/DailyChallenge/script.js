1e6153d5001ca2828fc5525d3edfd33fc095fcf1
lior shaked <Liorshak1986@gmail.com>
2021-10-12 22:05:10 +0300

    daily challange done

---
+++ Week2/Day3/DailyChallenge/script.js
@@ -0,0 +1,23 @@
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out “nested for loops” on Google).
+// Do this Daily Challenge by youself, without looking at the answers on the internet.
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *
+// * * * * * *
+
+for(let i=1; i<7;i++){
+    console.log("*".repeat(i))
+}
+
+let=kohavit="";
+
+for (let i=1;i<7;i++){
+    for (let b=0;b<i;b++){
+        kohavit+="*";
+    }
+    console.log(kohavit);
+    kohavit="";
+}
