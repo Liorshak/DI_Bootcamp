c21b8b972ec7464b141a0c843de04281d4fbfbf8
lior shaked <Liorshak1986@gmail.com>
2021-10-15 01:11:05 +0300

    Calculator ready

--- Week2/Day5/Calculator/script.js
+++ Week2/Day5/Calculator/script.js
@@ -1,3 +1,55 @@
-number(num);
-operator(operator);
-equal();
+// number(num);
+// operator(operator);
+// equal();
+
+let operators = ["+", "-", "/", "*"];
+let line = "";
+let ans = "";
+let res = "";
+let old = "";
+let def = "0";
+
+const number = (num) => {
+  line += num;
+  return (document.getElementById("baseLine").textContent = line);
+};
+
+const operator = (operator) => {
+  line += operator;
+  return (document.getElementById("baseLine").textContent = line);
+};
+
+const equal = () => {
+  if (operators.indexOf(line[0]) > -1) {
+    line = ans + line;
+  }
+
+  res = eval(line);
+  ans = res.toString();
+  line = "";
+
+  return (document.getElementById("baseLine").textContent = res);
+};
+
+const reset = () => {
+  line = "";
+  ans = "";
+  return (document.getElementById("baseLine").textContent = def);
+};
+
+const clean = () => {
+  line = "";
+
+  return (document.getElementById("baseLine").textContent = def);
+};
+
+const oldAns = () => {
+  if (ans.length > 0 && ans != undefined) {
+    line += ans;
+  }
+  if (line.length > 0) {
+    return (document.getElementById("baseLine").textContent = line);
+  } else {
+    return (document.getElementById("baseLine").textContent = def);
+  }
+};
