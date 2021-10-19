d4811054dac65a663d05eb54381af615633b2565
lior shaked <Liorshak1986@gmail.com>
2021-10-20 02:46:33 +0300

    Daily Challenge Ready

---
+++ Week3/Day3/Daily_challenge/script.js
@@ -0,0 +1,15 @@
+let inZone = document.getElementById("inZone");
+let show = document.getElementById("disp");
+
+let displaying = (event) => {
+  let lettersArr = event.target.value.split("");
+  let showArr = [];
+  lettersArr.forEach((v) => {
+    if (v.match(/^[A-Z]+$/i) || v == " ") {
+      showArr.push(v);
+    }
+  });
+  show.textContent = showArr.join("");
+};
+
+inZone.addEventListener("input", displaying);
