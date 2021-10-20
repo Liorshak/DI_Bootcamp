ae7e81cfaa958214c347250bb8f1df2df1bb3643
lior shaked <Liorshak1986@gmail.com>
2021-10-20 14:06:17 +0300

    Coloring game finished\

---
+++ Week3/Day4/color_game/script.js
@@ -0,0 +1,93 @@
+let colorMenu = document.getElementById("colorMenu");
+let main = document.getElementById("main");
+let content = document.getElementById("content");
+let clearBtn = document.getElementById("clearBtn");
+let sideBar = document.getElementById("sideBar");
+var myColor;
+var flagMouse;
+
+let setColor = (event) => {
+  myColor = event.target.style.background;
+  console.log("setting color");
+};
+
+let clickIn = (event) => {
+  flagMouse = true;
+  myColor = event.target.style.background;
+};
+let clickOut = () => {
+  flagMouse = false;
+};
+
+const createElementColor = (tag, color, location) => {
+  let newTag = document.createElement(tag);
+  newTag.style.background = color;
+  newTag.addEventListener("click", setColor);
+
+  location.appendChild(newTag);
+};
+
+main.addEventListener("mousedown", clickIn);
+main.addEventListener("mouseup", clickOut);
+
+let colorArray = [
+  "red",
+  "orangered",
+  "orange",
+  "yellow",
+  "yellowgreen",
+  "lightgreen",
+  "green",
+  "turquoise",
+  "cyan",
+  "lightskyblue",
+  "dogerblue",
+  "blue",
+  "darkblue",
+  "indigo",
+  "darkmagenta",
+  "violet",
+  "lightpink",
+  "lightgray",
+  "gray",
+  "black",
+  "white",
+];
+
+colorArray.forEach((v) => {
+  console.log(v);
+  createElementColor("div", v, colorMenu);
+});
+
+let paintC = (event) => {
+  event.target.style.background = myColor;
+};
+
+let paintO = (event) => {
+  if (flagMouse) {
+    event.target.style.background = myColor;
+  }
+};
+
+const createElement = (tag, location) => {
+  newTag = document.createElement(tag);
+  newTag.classList.add("board");
+  newTag.addEventListener("click", paintC);
+  newTag.addEventListener("mouseover", paintO);
+  location.appendChild(newTag);
+};
+
+for (let i = 0; i < 60 * 24; i++) {
+  createElement("div", content);
+}
+
+let paintBoard = document.getElementsByClassName("board");
+console.log(paintBoard);
+
+clearingAll = () => {
+  for (let board of paintBoard) {
+    board.style.background = "white";
+  }
+};
+
+clearBtn.addEventListener("click", clearingAll);
