5a910e1f9926d006d343b5fef7afe55ebcdf1eb1
lior shaked <Liorshak1986@gmail.com>
2021-10-28 02:59:28 +0300

    finished Ninja

---
+++ Week4/Day4/Ninja/script.js
@@ -0,0 +1,18 @@
+// class Bird {
+//     constructor() {
+//       console.log("I'm a bird. 🦢");
+//     }
+//   }
+
+//   class Flamingo extends Bird {
+//     constructor() {
+//       console.log("I'm pink. 🌸");
+//       super();
+//     }
+//   }
+
+//   const pet = new Flamingo();
+
+"The Out Put will be !";
+"I'm pink. 🌸";
+"I'm a bird. 🦢";
