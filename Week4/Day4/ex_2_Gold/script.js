098e87e6117b074e45761bfcf69eef21b655f84f
lior shaked <Liorshak1986@gmail.com>
2021-10-28 03:24:25 +0300

    gold almost finished

---
+++ Week4/Day4/ex_2_Gold/script.js
@@ -0,0 +1,48 @@
+//part1
+
+let printFullName = ({ first, last }) =>
+  console.log(`Your full name is ${first} ${last}`);
+
+printFullName({ first: "Elie", last: "Schoppik" });
+
+//part 2
+
+// Examples;
+// keysAndValues({ a: 1, b: 2, c: 3 })
+// ➞ [["a", "b", "c"], [1, 2, 3]]
+
+// keysAndValues({ a: "Apple", b: "Microsoft", c: "Google" })
+// ➞ [["a", "b", "c"], ["Apple", "Microsoft", "Google"]]
+
+// keysAndValues({ key1: true, key2: false, key3: undefined })
+// ➞ [["key1", "key2", "key3"], [true, false, undefined]]
+
+keysAndValues = ({ ...obj }) => [
+  object.keys(obj).sort(),
+  object.values(obj).sort(),
+];
+
+console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
+console.log(keysAndValues({ a: "Apple", b: "Microsoft", c: "Google" }));
+console.log(keysAndValues({ key1: true, key2: false, key3: undefined }));
+
+// part 3
+
+// class Counter {
+//   constructor() {
+//     this.count = 0;
+//   }
+
+//   increment() {
+//     this.count++;
+//   }
+// }
+
+// const counterOne = new Counter();
+// counterOne.increment(); //1
+// counterOne.increment(); //2
+
+// const counterTwo = counterOne;  //equal by reference
+// counterTwo.increment();  //3
+
+// console.log(counterOne.count);  //its the same obj as counter 2 so it will be 3!
