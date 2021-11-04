c04f31e01de1a9592ec94f9f007067654abe7713
lior shaked <Liorshak1986@gmail.com>
2021-11-04 22:31:54 +0200

    cleaning all console.log

--- Week5/Day5/Daily_challenge/script.js
+++ Week5/Day5/Daily_challenge/script.js
@@ -5,7 +5,6 @@
 
 let data;
 let identifier;
-
 let from = document.getElementById("from");
 let to = document.getElementById("to");
 
@@ -14,7 +13,6 @@ let getData = async () => {
     "http://api.currencylayer.com/live?access_key=6b543e9294a71d26a6606f5449cf71a6&format=1",
     "https://pkgstore.datahub.io/core/currency-codes/codes-all_json/data/029be9faf6547aba93d64384f7444774/codes-all_json.json",
   ];
-
   let dataList = await Promise.all(
     urls.map((urls) => {
       return fetch(urls)
@@ -22,7 +20,6 @@ let getData = async () => {
         .catch((err) => console.log(err));
     })
   );
-
   data = dataList[0].quotes;
   identifier = dataList[1];
   addSelection(data, identifier);
@@ -31,31 +28,23 @@ let getData = async () => {
 getData();
 
 function addSelection(data, identifier) {
-  console.log(data, identifier);
   let CurrenciesARR = Object.keys(data).filter((v) => v != null);
   let identifierARR = identifier.filter((v) => v.AlphabeticCode != null);
-  console.log(CurrenciesARR);
-  console.log(identifierARR);
   let selectOption = CurrenciesARR.map((v) => {
     let index = identifierARR.findIndex((x) => {
-      console;
       if (`USD${x.AlphabeticCode}` === v) return true;
     });
-
-    console.log(index);
     if (index >= 0) {
       return [identifierARR[index].Currency, v];
     }
   });
 
   selectOption = selectOption.filter((v) => v != undefined);
-
   selectOption.forEach((v) => {
     newFrom = document.createElement("option");
     newFrom.appendChild(document.createTextNode(v[0]));
     newFrom.setAttribute("value", v[1]);
     from.appendChild(newFrom);
-
     newTo = document.createElement("option");
     newTo.appendChild(document.createTextNode(v[0]));
     newTo.setAttribute("value", v[1]);
@@ -66,23 +55,14 @@ function addSelection(data, identifier) {
 function converter(event) {
   event.preventDefault();
   let amount = document.getElementById("amount");
-
-  if (from.value.length > 0 && to.value.length > 0) {
-    if (amount.value.length > 0) {
-      amount = parseFloat(amount.value);
-      console.log(amount);
-      console.log(data[from.value]);
-      console.log(data[to.value]);
-      let total = (amount / data[from.value]) * data[to.value];
-
-      let result = document.getElementById("result");
-      result.textContent = `${
-        Math.round(total * 100) / 100
-      } ${to.value.substring(3)}`;
-    } else {
-      alert("Please enter valid amount of currency.");
-    }
+  if (amount.value.length > 0) {
+    amount = parseFloat(amount.value);
+    let total = (amount / data[from.value]) * data[to.value];
+    let result = document.getElementById("result");
+    result.textContent = `${Math.round(total * 100) / 100} ${to.value.substring(
+      3
+    )}`;
   } else {
-    alert("Please select FROM currency and TO currency");
+    alert("Please enter valid amount of currency.");
   }
 }
