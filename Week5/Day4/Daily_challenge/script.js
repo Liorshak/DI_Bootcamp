b1d3022035fa39c44e36b8add5cd0cf9a6660f54
lior shaked <Liorshak1986@gmail.com>
2021-11-04 07:56:59 +0200

    daily challenge finished

---
+++ Week5/Day4/Daily_challenge/script.js
@@ -0,0 +1,9 @@
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "foo");
+});
+// expected output: Array [3, 42, "foo"]
+Promise.all([promise1, promise2, promise3])
+  .then(console.log)
+  .catch((err) => console.log(err));
