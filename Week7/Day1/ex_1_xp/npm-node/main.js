08e0afc64be3feff952465f9e21ddf061d54b8af
lior shaked <Liorshak1986@gmail.com>
2021-11-15 01:03:35 +0200

    first ex XP finished

---
+++ Week7/Day1/ex_1_xp/npm-node/main.js
@@ -0,0 +1,13 @@
+// console.log("Hi, thanks for running me with npm start!");
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+// let talk = `Hello you are ${a + b}`;
+// console.log(talk);
+
+console.log(`first run: ${x}`);
+console.log(`second run: ${x + c}`);
